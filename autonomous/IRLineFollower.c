85f55ba607aa05223c64e1c0ffd2371fd368771b
aidanwood <aidanwood5@outlook.com>
2014-11-03 15:35:53 -0800

    Correct team name

--- autonomous/IRLineFollower.c
+++ autonomous/IRLineFollower.c
@@ -7,21 +7,21 @@
 
 	(C) Copyright 2014 Alex Jordan
 
-	This file is part of the FTC team 6157 application code.
+	This file is part of the FTC team 5619 application code.
 
-	FTC team 6157 application code is free software: you can
+	FTC team 5619 application code is free software: you can
 	redistribute it and/or modify it under the terms of the GNU
 	General Public License as published by the Free Software
 	Foundation, either version 2 of the License, or (at your
 	option) any later version.
 
-	FTC team 6157 application code is distributed in the hope that
+	FTC team 5619 application code is distributed in the hope that
 	it will be useful, but WITHOUT ANY WARRANTY; without even the
 	implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
 	PURPOSE.  See the GNU General Public License for more details.
 
 	You should have received a copy of the GNU General Public
-	License along with FTC team 6157 application code. If not, see
+	License along with FTC team 5619 application code. If not, see
 	<http://www.gnu.org/licenses/>.
 
 */
