4867b331c00ea974bc3cb1e21102e76dc483a28e
Willy Felton <wfelton@seattleacademy.org>
2014-11-11 10:19:44 -0800

    Finish 5619Drive.h and teleop_wf.c

--- teleop/5619Drive.h
+++ teleop/5619Drive.h
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 #pragma config(Sensor, S1, Motor, sensorI2CCustom)
 
 #include "Motors.h"
@@ -10,19 +11,39 @@ int rightMotorNumber=2;
 //Notice that motor (S3,1,2) is wired backwards so a positive value is back
 // (S3,1,1) = left motors
 // (S3,1,2) = right motors (backward)
-
+bool TESTBOT=true;
+bool DEBUG=true;
 //**********************************************
 //Wrapper functions for left/right/center motors
 void leftMotor(int spd){
-	Motors_SetSpeed(MotorController, leftMotorDaisyChainLevel, leftMotorNumber, spd);
+	if (TESTBOT){
+		writeDebugStreamLine("LEFT POWER %d",spd);
+		Sleep(1000);
+	}
+	else {
+		Motors_SetSpeed(MotorController, leftMotorDaisyChainLevel, leftMotorNumber, spd);
+	}
+
 }
 
 void rightMotor(int spd){
-	Motors_SetSpeed(MotorController, rightMotorDaisyChainLevel, rightMotorNumber, spd);
+	if (TESTBOT){
+		writeDebugStreamLine("RIGHT POWER %d",spd);
+		Sleep(1000);
+	}
+	else {
+		Motors_SetSpeed(MotorController, rightMotorDaisyChainLevel, rightMotorNumber, spd);
+	}
 }
 
 void centerMotor(int spd){
+	if (TESTBOT){
+		writeDebugStreamLine("CENTER POWER %d",spd);
+		Sleep(1000);
+	}
+	else {
 		Motors_SetSpeed(MotorController,2,2,spd);
+	}
 }
 //end wrapper functions
 //****************************************************************
@@ -31,48 +52,62 @@ void centerMotor(int spd){
 //Spin Functions: Gives opposite speeds to left vs right
 void Drive_spinRight(int speed)
 {
+	if (DEBUG){
+		writeDebugStreamLine("in Drive_spinRight");
+	}
 	leftMotor(speed);
 	rightMotor(speed);
 }
 
 void Drive_spinLeft(int speed)
 {
+	if (DEBUG){
+		writeDebugStreamLine("in Drive_spinLeft");
+	}
 	leftMotor(-speed);
 	rightMotor(-speed);
 }
 //*****************End Spin Functions******************************
 
-//turn is a catch all turn function that will act like a 
+//turn is a catch all turn function that will act like a
 //tank drive.  The input are the speeds of the two sides of the
 //robot.  The calling function has to manage their power difference
 //Values can be + or - but the right side will be inverted per the wiring of the motors
 void Drive_turn(int leftSpeed, int rightSpeed){
+	if (DEBUG){
+		writeDebugStreamLine("in Drive_turn");
+	}
 	leftMotor(leftSpeed);
 	rightMotor(-rightSpeed);
 }
 
 //********************************************************************
-// The forward and backward functions are not ncessary but make 
+// The forward and backward functions are not ncessary but make
 // the code more readible
 
-// Forward takes ONLY A POSITIVE VALUE assuming that 
+// Forward takes ONLY A POSITIVE VALUE assuming that
 //we want to go forward
 void Drive_forward(int speed)
 {
+	if (DEBUG){
+		writeDebugStreamLine("in Drive_forward");
+	}
 	speed=abs(speed);  //positive value check
 	leftMotor(speed);
 	rightMotor(-speed);
 }
-// Backward takes ONLY A POSITIVE VALUE assuming that 
+// Backward takes ONLY A POSITIVE VALUE assuming that
 //we want to go backward
 void Drive_backward (int speed) {
+	if (DEBUG){
+		writeDebugStreamLine("in Drive_backward");
+	}
 	speed=abs(speed);  //positive value check
 	leftMotor(-speed);
 	rightMotor(speed);
 }
 //**********************End forward/backward**********************
-
-// driveOmni takes EITHER a + or - for either speed and this will 
+// driveOmni takes EITHER a + or - for either speed and this will
 //manage the direction
 //These values should come from the analog control values
 //QUESTION: Is the positive value for the center wheel left or right?
@@ -89,7 +124,7 @@ void Drive_driveOmni(int inLineSpeed, int centerSpeed){
 }
 
 //Kills all motors
-void Drive_allStop(){	
+void Drive_allStop(){
 	centerMotor(0);
 	leftMotor(0);
 	rightMotor(0);
