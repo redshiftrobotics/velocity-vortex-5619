7813b33da5da181776274c86cf6e14e7551b8fc2
Noah Rose Ledesma <NoahRoseLedesma@seattleacademy.org>
2015-10-31 14:05:55 -0700

    Append scale_motor_power
    Scaling function.

--- FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/OpModeTest.java
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/OpModeTest.java
@@ -78,5 +78,51 @@ public class OpModeTest extends OpMode
         }
         return dscale;
     }
+    
+    double scale_motor_power (double p_power)
+    {
+        //
+        // Assume no scaling.
+        //
+        double l_scale = 0.0f;
+
+        //
+        // Ensure the values are legal.//
+        double l_power = Range.clip (p_power, -1, 1);
+
+        double[] l_array =
+                { 0.00, 0.05, 0.09, 0.10, 0.12
+                        , 0.15, 0.18, 0.24, 0.30, 0.36
+                        , 0.43, 0.50, 0.60, 0.72, 0.85
+                        , 1.00, 1.00
+                };
+
+        //
+        // Get the corresponding index for the specified argument/parameter.
+        //
+        int l_index = (int) (l_power * 16.0);
+        if (l_index < 0)
+        {
+            l_index = -l_index;
+        }
+        else if (l_index > 16)
+        {
+            l_index = 16;
+        }
+
+        if (l_power < 0)
+        {
+            l_scale = -l_array[l_index];
+        }
+        else
+        {
+            l_scale = l_array[l_index];
+        }
+
+        return l_scale;
+
+    } // PushBotManual::scale_motor_power
+
+}
 
 }
