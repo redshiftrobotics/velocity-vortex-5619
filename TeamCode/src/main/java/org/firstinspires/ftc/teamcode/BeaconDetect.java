ebde7a66fc2e489ab2dbe707345c523f0f3effbb
Ari Porad <ari@ariporad.com>
2016-09-27 16:50:51 -0700

    feat: first working beacon detector

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BeaconDetect.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BeaconDetect.java
@@ -1,50 +1,87 @@
 package org.firstinspires.ftc.teamcode;
 
-import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import android.graphics.Bitmap;
+import android.graphics.Matrix;
+
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
+import java.util.HashMap;
 
-@TeleOp(name="Beacon Detect", group="Dev")
-public class BeaconDetect extends OpMode
-{
+import for_camera_opmodes.OpModeCamera;
 
-    /*
-     * Code to run ONCE when the driver hits INIT
-     */
-    @Override
-    public void init() {
-        telemetry.addData("Status", "Initialized");
+/**
+ * TeleOp Mode
+ * <p/>
+ * Enables control of the robot via the gamepad
+ */
+
+@TeleOp(name = "Beacon Detect", group = "Dev")
+public class BeaconDetect extends OpModeCamera {
+
+  int ds2 = 4;  // additional downsampling of the image
+  private BeaconDetector detector;
+  // set to 1 to disable further downsampling
+
+  /*
+   * Code to run when the op mode is first enabled goes here
+   * @see com.qualcomm.robotcore.eventloop.opmode.OpMode#start()
+   */
+  @Override
+  public void init() {
+    HashMap<String, Integer> options = new HashMap<String, Integer>();
+    options.put("red:r"         , 210);
+    options.put("red:g"         , 150);
+    options.put("red:b"         , 240);
+    options.put("blue:r"        ,  85);
+    options.put("blue:g"        , 255);
+    options.put("blue:b"        , 200);
+    options.put("classify:main" , 210);
+    options.put("classify:other", 210);
+
+    detector = new BeaconDetector(options);
+
+    setCameraDownsampling(2);
+    // parameter determines how downsampled you want your images
+    // 8, 4, 2, or 1.
+    // higher number is more downsampled, so less resolution but faster
+    // 1 is original resolution, which is detailed but slow
+    // must be called before super.init sets up the camera
+
+    super.init(); // inits camera functions, starts preview callback
+  }
+
+  /*
+   * This method will be called repeatedly in a loop
+   * @see com.qualcomm.robotcore.eventloop.opmode.OpMode#loop()
+   */
+  @Override
+  public void loop() {
+
+    if (gamepad1.x) {
+      telemetry.addData("Status", "Processing");
+      while (!imageReady()) {} // wait for an image from the camera
+
+      Bitmap image = convertYuvImageToRgb(yuvImage, width, height, ds2);
+
+      // https://eventuallyconsistent.net/2011/07/21/rotating-a-bitmap-in-android/
+      // create a matrix object
+      Matrix matrix = new Matrix();
+      matrix.postRotate(-90); // clockwise by 90 degrees
+
+      // create a new bitmap from the original using the matrix to transform the result
+      image = Bitmap.createBitmap(image, 0, 0, image.getWidth(), image.getHeight(), matrix, true);
+
+      telemetry.addData("State", detector.detect(image).toString());
+      telemetry.addData("Status", "Complete");
+
+      while (gamepad1.x) {} // Wait for the button to be released
     }
 
-    /*
-     * Code to run REPEATEDLY after the driver hits INIT, but before they hit PLAY
-     */
-    @Override
-    public void init_loop() {
-    }
 
-    /*
-     * Code to run ONCE when the driver hits PLAY
-     */
-    @Override
-    public void start() {
-        telemetry.addData("Status", "Started");
-    }
+  }
 
-    /*
-     * Code to run REPEATEDLY after the driver hits PLAY but before they hit STOP
-     */
-    @Override
-    public void loop() {
-        telemetry.addData("Status", "Running");
-    }
-
-    /*
-     * Code to run ONCE after the driver hits STOP
-     */
-    @Override
-    public void stop() {
-        telemetry.addData("Status", "Stopped");
-    }
-
+  @Override
+  public void stop() {
+    super.stop(); // stops camera functions
+  }
 }
