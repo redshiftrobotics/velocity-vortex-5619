ff08573c4053fe87739c08192aefac884240b3d3
egold555 <eric@golde.org>
2016-09-27 15:53:46 -0700

    Config and Util
    RedshiftConfig and some useful config funtions

---
+++ TeamCode/src/main/java/org/redshiftrobotics/config/ConfigVariable.java
@@ -0,0 +1,46 @@
+package org.redshiftrobotics.config;
+
+public class ConfigVariable {
+    public final String name;
+    double value;
+    double min;
+    double max;
+    double increment;
+    protected ConfigType type = ConfigType.DOUBLE;
+
+    public ConfigVariable(String name, double value, double min, double max, double increment){
+        this.name = name;
+        this.value = value;
+        this.min = min;
+        this.max = max;
+        this.increment = increment;
+    }
+
+    public boolean getValueBoolean(){
+        if(value > 0) {
+            return true;
+        }
+        return false;
+    }
+
+    public ConfigVariable setType(ConfigType type){
+        this.type = type;
+        return this;
+    }
+
+    public int getValueInt(){
+        return (int) Math.round(value);
+    }
+
+    public double getValueDouble(){
+        return value;
+    }
+
+    public float getValueFloat(){
+        return (float) value;
+    }
+
+    public void setValue(double value){
+        this.value = value;
+    }
+}
