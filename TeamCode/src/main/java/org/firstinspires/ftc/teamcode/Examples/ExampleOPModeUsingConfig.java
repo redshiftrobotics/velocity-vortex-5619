447d9aeabeb2227de373ee7404a0b3a8c8cb92d4
egold555 <eric@golde.org>
2016-10-13 16:48:29 -0700

    Updated example Config
    Now the example config shows color and colorRaw.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Examples/ExampleOPModeUsingConfig.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Examples/ExampleOPModeUsingConfig.java
@@ -5,6 +5,8 @@ import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 import org.redshiftrobotics.config.ConfigVariable;
+import org.redshiftrobotics.util.ColorPicker;
+import org.redshiftrobotics.util.ColorPicker.Color;
 import org.redshiftrobotics.util.Util;
 
 import org.redshiftrobotics.config.ConfigType;
@@ -22,6 +24,7 @@ public class ExampleOPModeUsingConfig extends OpMode {
     static final ConfigVariable test2 = new ConfigVariable("float", 3.3, 1.111, 400.48, 3.6).setType(ConfigType.FLOAT);
     static final ConfigVariable test3 = new ConfigVariable("double", 3.3, 1.111, 400.48, 3.6).setType(ConfigType.DOUBLE);
     static final ConfigVariable test4 = new ConfigVariable("boolean", 1, 0, 1, 1).setType(ConfigType.BOOLEAN);
+    static final ConfigVariable test5 = new ConfigVariable("color", Color.BLUE.getNumber(), (double)Color.MIN, (double)Color.MAX, 1).setType(ConfigType.COLORPICKER);
     Util util = new Util(this);
 
     static void initMe() //you need this because static { } does not get called in FTC //CALL CLEAR BEFORE ANYTHING
@@ -31,6 +34,7 @@ public class ExampleOPModeUsingConfig extends OpMode {
         ExampleConfig.config.addVariable(test2);
         ExampleConfig.config.addVariable(test3);
         ExampleConfig.config.addVariable(test4);
+        ExampleConfig.config.addVariable(test5);
     }
 
     @Override
@@ -41,10 +45,12 @@ public class ExampleOPModeUsingConfig extends OpMode {
     @Override
     public void start(){
         //for this simple op mode just spit out the data
-        util.log("int: " + test1.getValueInt());
-        util.log("float: " + test2.getValueFloat());
-        util.log("double: " + test3.getValueDouble());
-        util.log("boolean: " + test4.getValueBoolean());
+        util.log("int" + test1.getValueInt());
+        util.log("float" + test2.getValueFloat());
+        util.log("double" + test3.getValueDouble());
+        util.log("boolean" + test4.getValueBoolean());
+        util.log("color" + test5.getValueColor().getName());
+        util.log("colorRaw" + test5.getValueColor().getNumber());
         util.updateTelemetry();
     }
 
