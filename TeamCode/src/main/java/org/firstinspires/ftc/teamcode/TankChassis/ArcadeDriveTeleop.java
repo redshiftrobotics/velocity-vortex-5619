1c8c5e02e7a6aa4827f3cc823536c7c1ce5948c8
egold555 <eric@golde.org>
2016-10-13 17:09:12 -0700

    Teleop works
    Removed useless confusing files that no one wants at all and got tank
    and arcade teleop fixed

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TankChassis/ArcadeDriveTeleop.java
@@ -0,0 +1,45 @@
+package org.firstinspires.ftc.teamcode.TankChassis;
+
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.util.Range;
+
+
+/**
+ * Created by Eric Golde on 9/20/2016.
+ */
+
+//simple arcade drive. Use joystick #1
+@TeleOp(name="ArcadeDrive", group="TankChassis")
+public class ArcadeDriveTeleop extends TankChassisBase {
+
+    @Override
+    public void init() {
+        super.init();
+    }
+
+    @Override
+    public void start(){
+
+    }
+
+    @Override
+    public void loop() {
+
+        float xValue = gamepad1.left_stick_x;
+        float yValue = -gamepad1.left_stick_y;
+
+        float leftPower = yValue + xValue;
+        float rightPower = yValue - xValue;
+
+        leftPower = Range.clip(leftPower, -TankChassisConfig.motorSpeed.getValueFloat(), TankChassisConfig.motorSpeed.getValueFloat());
+        rightPower = Range.clip(rightPower, -TankChassisConfig.motorSpeed.getValueFloat(), TankChassisConfig.motorSpeed.getValueFloat());
+
+        left.setPower(leftPower);
+        right.setPower(rightPower);
+    }
+
+    @Override
+    public void stop(){
+
+    }
+}
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TankChassis/TankDriveTeleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TankChassis/TankDriveTeleop.java
@@ -3,8 +3,6 @@ package org.firstinspires.ftc.teamcode.TankChassis;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.util.Range;
 
-import org.firstinspires.ftc.teamcode.TankArcadeDrive.DriveOPBase;
-
 /**
  * Created by Eric Golde on 10/8/2016.
  */
@@ -26,8 +24,8 @@ public class TankDriveTeleop extends TankChassisBase {
         float rightValue = -gamepad1.right_stick_y;
 
         //clip the variables to be what the config says
-        leftValue = Range.clip(leftValue, TankChassisConfig.motorSpeed.getValueFloat(), TankChassisConfig.motorSpeed.getValueFloat());
-        rightValue = Range.clip(rightValue, TankChassisConfig.motorSpeed.getValueFloat(), TankChassisConfig.motorSpeed.getValueFloat());
+        leftValue = Range.clip(leftValue, -TankChassisConfig.motorSpeed.getValueFloat(), TankChassisConfig.motorSpeed.getValueFloat());
+        rightValue = Range.clip(rightValue, -TankChassisConfig.motorSpeed.getValueFloat(), TankChassisConfig.motorSpeed.getValueFloat());
 
         left.setPower(leftValue);
         right.setPower(rightValue);
