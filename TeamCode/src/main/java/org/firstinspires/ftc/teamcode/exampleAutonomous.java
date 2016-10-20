62b2e7f79d87a8c7699b13827787697bc0460dee
noahroseledesma <noahroseledesma@seattleacademy.org>
2016-10-20 09:59:12 -0700

    feat(PID-Drive): Add PID-Drive

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/exampleAutonomous.java
@@ -0,0 +1,38 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.hardware.I2cDeviceSynch;
+
+
+@Autonomous(name = "exampleAutonomous", group = "Autonomous")
+public class exampleAutonomous extends LinearOpMode {
+
+    robot myRobot;
+    I2cDeviceSynch imu;
+    DcMotor leftDrive;
+    DcMotor rightDrive;
+
+    @Override
+    public void runOpMode(){
+
+        // Initialize our hardware
+        leftDrive = hardwareMap.dcMotor.get("left_drive");
+        rightDrive = hardwareMap.dcMotor.get("right_drive");
+        imu = hardwareMap.i2cDeviceSynch.get("imu");
+
+        // Create our driver
+        myRobot = new robot(imu, leftDrive, rightDrive);
+
+        myRobot.setPowerMultiplier(3/8f);
+        // Set our p, i, and d tuning
+        myRobot.Data.PID.pTuning = 37f;
+        myRobot.Data.PID.iTuning = 1f;
+        myRobot.Data.PID.dTuning = 4f;
+
+        // Lets Drive straight ten rotations or 10 seconds.
+        myRobot.Straight(10f, 10);
+    }
+}
+
