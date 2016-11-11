395f1216d3b248a673a9a3e8cbc5d990e11b9a38
noahroseledesma <noahroseledesma@seattleacademy.org>
2016-11-11 04:07:51 -0800

    style(PID-Drive): Make members camelCalse and classes UpperCamelCase

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ExampleAutonomous.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ExampleAutonomous.java
@@ -7,10 +7,10 @@ import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.hardware.I2cDeviceSynch;
 
 @Disabled
-@Autonomous(name = "exampleAutonomous", group = "Autonomous")
-public class exampleAutonomous extends LinearOpMode {
+@Autonomous(name = "ExampleAutonomous", group = "Autonomous")
+public class ExampleAutonomous extends LinearOpMode {
 
-    robot myRobot;
+    Robot myRobot;
     I2cDeviceSynch imu;
     DcMotor leftDrive;
     DcMotor rightDrive;
@@ -24,15 +24,15 @@ public class exampleAutonomous extends LinearOpMode {
         imu = hardwareMap.i2cDeviceSynch.get("imu");
 
         // Create our driver
-        myRobot = new robot(imu, leftDrive, rightDrive);
+        myRobot = new Robot(imu, leftDrive, rightDrive);
 
         // Set our p, i, and d tuning
-        myRobot.Data.PID.PTuning = 37f;
-        myRobot.Data.PID.ITuning = 1f;
-        myRobot.Data.PID.DTuning = 4f;
+        myRobot.data.PID.PTuning = 37f;
+        myRobot.data.PID.ITuning = 1f;
+        myRobot.data.PID.DTuning = 4f;
 
-        // Lets Drive straight ten rotations or 10 seconds.
-        myRobot.Straight(10f, 10);
+        // Lets drive straight ten rotations or 10 seconds.
+        myRobot.straight(10f, 10);
 		// Now do a 90 degree turn
 		myRobot.AngleTurn(90, 5);
     }
